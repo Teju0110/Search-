0908026ac50ab742002aea5fe1e64c3e87118cec
tejasvisawant1998@gmail.com <tejasvisawant1998@gmail.com>
2022-11-29 20:15:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import {data} from './data.js'
 
 function App() {
+
+  const[search,setSearch]=useState("")
+  const handleSearch=()=>{
+    return data.filter((name)=>(
+      name.first_name.toLowerCase().includes(search)||
+      name.last_name.toLowerCase().includes(search)||
+      name.email.toLowerCase().includes(search)
+    ))
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <input type="text" placeholder="Search...." className='search' onChange={(e)=>{setSearch(e.target.value)}}/>
+      <div className='data'>
+      {
+      handleSearch().map(data=>(
+          <div className='data_item'>
+            <h3>Name :{data.first_name} {data.last_name}</h3>
+            <h4>Email: {data.email}</h4>
+            <h4>Gender: {data.gender}</h4>
+          </div>
+        ))
+      }
+      </div>
     </div>
   );
 }
